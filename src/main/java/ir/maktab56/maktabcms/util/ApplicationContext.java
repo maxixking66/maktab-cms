79c868617d7d775e1d922e29066d7ee939fe228e
maxixking66 <maxixking66@gmail.com>
2021-09-16 11:24:51 +0430

    create beans

--- src/main/java/ir/maktab56/maktabcms/util/ApplicationContext.java
+++ src/main/java/ir/maktab56/maktabcms/util/ApplicationContext.java
@@ -1,15 +1,31 @@
 package ir.maktab56.maktabcms.util;
 
+import ir.maktab56.maktabcms.repository.MediaCategoryRepository;
+import ir.maktab56.maktabcms.repository.impl.MediaCategoryRepositoryImpl;
+import ir.maktab56.maktabcms.service.MediaCategoryService;
+import ir.maktab56.maktabcms.service.impl.MediaCategoryServiceImpl;
+
 import javax.persistence.EntityManager;
 
 public class ApplicationContext {
 
+    private static final MediaCategoryRepository mediaCategoryRepository;
+    private static final MediaCategoryService mediaCategoryService;
+
     static {
-        EntityManager entityManager = HibernateUtil.getEntityMangerFactory().createEntityManager();
+        EntityManager entityManager = HibernateUtil.getTestEntityMangerFactory().createEntityManager();
+        mediaCategoryRepository = new MediaCategoryRepositoryImpl(entityManager);
+        mediaCategoryService = new MediaCategoryServiceImpl(mediaCategoryRepository);
     }
 
     private ApplicationContext() {
     }
 
+    public static MediaCategoryRepository getMediaCategoryRepository() {
+        return mediaCategoryRepository;
+    }
 
+    public static MediaCategoryService getMediaCategoryService() {
+        return mediaCategoryService;
+    }
 }
