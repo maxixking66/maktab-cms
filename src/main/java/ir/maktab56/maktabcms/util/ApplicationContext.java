e32ee039035e68b6d8485fe1dfee17b6d4e0ac13
maxixking66 <maxixking66@gmail.com>
2021-09-16 14:31:34 +0430

    fill ApplicationContext

--- src/main/java/ir/maktab56/maktabcms/util/ApplicationContext.java
+++ src/main/java/ir/maktab56/maktabcms/util/ApplicationContext.java
@@ -1,9 +1,15 @@
 package ir.maktab56.maktabcms.util;
 
 import ir.maktab56.maktabcms.repository.MediaCategoryRepository;
+import ir.maktab56.maktabcms.repository.UserRepository;
 import ir.maktab56.maktabcms.repository.impl.MediaCategoryRepositoryImpl;
+import ir.maktab56.maktabcms.repository.impl.UserRepositoryImpl;
+import ir.maktab56.maktabcms.service.AuthenticationProvider;
 import ir.maktab56.maktabcms.service.MediaCategoryService;
+import ir.maktab56.maktabcms.service.UserService;
+import ir.maktab56.maktabcms.service.impl.AuthenticationProviderImpl;
 import ir.maktab56.maktabcms.service.impl.MediaCategoryServiceImpl;
+import ir.maktab56.maktabcms.service.impl.UserServiceImpl;
 
 import javax.persistence.EntityManager;
 
@@ -11,11 +17,17 @@ public class ApplicationContext {
 
     private static final MediaCategoryRepository mediaCategoryRepository;
     private static final MediaCategoryService mediaCategoryService;
+    private static final UserRepository userRepository;
+    private static final UserService userService;
+    private static final AuthenticationProvider authenticationProvider;
 
     static {
         EntityManager entityManager = HibernateUtil.getTestEntityMangerFactory().createEntityManager();
         mediaCategoryRepository = new MediaCategoryRepositoryImpl(entityManager);
         mediaCategoryService = new MediaCategoryServiceImpl(mediaCategoryRepository);
+        userRepository = new UserRepositoryImpl(entityManager);
+        userService = new UserServiceImpl(userRepository);
+        authenticationProvider = new AuthenticationProviderImpl(userService);
     }
 
     private ApplicationContext() {
@@ -27,5 +39,17 @@ public class ApplicationContext {
 
     public static MediaCategoryService getMediaCategoryService() {
         return mediaCategoryService;
+    }
+
+    public static UserRepository getUserRepository() {
+        return userRepository;
+    }
+
+    public static UserService getUserService() {
+        return userService;
+    }
+
+    public static AuthenticationProvider getAuthenticationProvider() {
+        return authenticationProvider;
     }
 }
