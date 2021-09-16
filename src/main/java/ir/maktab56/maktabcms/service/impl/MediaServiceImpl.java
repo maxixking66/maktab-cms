b75b5592d4ea3e3632a35f5ac559dd0c02b2f2f1
maxixking66 <maxixking66@gmail.com>
2021-09-16 14:42:42 +0430

    override save

--- src/main/java/ir/maktab56/maktabcms/service/impl/MediaServiceImpl.java
+++ src/main/java/ir/maktab56/maktabcms/service/impl/MediaServiceImpl.java
@@ -2,12 +2,31 @@ package ir.maktab56.maktabcms.service.impl;
 
 import ir.maktab56.maktabcms.base.service.impl.BaseEntityServiceImpl;
 import ir.maktab56.maktabcms.domain.Media;
+import ir.maktab56.maktabcms.domain.User;
 import ir.maktab56.maktabcms.repository.MediaRepository;
 import ir.maktab56.maktabcms.service.MediaService;
+import ir.maktab56.maktabcms.util.SecurityContext;
 
 public class MediaServiceImpl extends BaseEntityServiceImpl<Media, Long, MediaRepository> implements MediaService {
 
     public MediaServiceImpl(MediaRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public Media save(Media media) {
+        User currentUser = SecurityContext.getCurrentUser();
+        if (currentUser == null) {
+            throw new RuntimeException("access denied!!!");
+        }
+        if (media.getId() == null) {
+            repository.getEntityManager().getTransaction().begin();
+            media = repository.save(media);
+            media.setUser(currentUser);
+            repository.getEntityManager().getTransaction().commit();
+            return media;
+        } else {
+            return super.save(media);
+        }
     }
 }
